35f053764af82b8af9cf0d91ef29d9345e328d04
Rocky Lee <89844895+DontActuallyKnow@users.noreply.github.com>
2023-01-14 12:10:40 +0000

    Create SpotifyLyricStatus.plugin.js

---
+++ SpotifyLyricStatus.plugin.js
@@ -0,0 +1,19 @@
+/**
+ * @name SpotifyLyricsStatus
+ * @author TooMuchIq
+ * @description Changes your status to the lyrics of the song you are currently listening to.
+ * @version 0.0.1
+ * @updateUrl 
+ */
+
+const start = () => {
+
+}
+
+const stop = () => {
+
+}
+
+module.exports = function() {};
+module.exports.start = start;
+module.exports.stop = stop;
